194bd9215f46fc65b1830d776ec445408fccdbc4
Koki Teramoto <terarin@terarin-iw.net>
2025-09-26 22:39:39 +0900

    [add]LED flash is added.

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,23 @@ use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::gpio::Pull;
 use esp_idf_hal::peripherals::Peripherals;
 
-fn main() {
+fn main()-> Result<(), Box<dyn std::error::Error>>{
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
-    let peripherals = Peripherals::take().unwrap();
-
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
-
-    log::info!("Hello, world!");
-    let mut pin = PinDriver::output(peripherals.pins.gpio32);
+    //test
+    let peripherals = Peripherals::take().unwrap();
+    let mut input = PinDriver::input(peripherals.pins.gpio32)?;
+    let mut output = PinDriver::input_output(peripherals.pins.gpio22)?;
+    input.set_pull(Pull::Up)?;
     loop {
-        if pin.is_low() {
-            log::info!("Landing is detected.");
+        if input.is_high() {
+            output.set_high()?;
+        } else {
+            output.set_low()?;
         }
-        FreeRtos::delay_ms(1000);
     }
+     
 }
